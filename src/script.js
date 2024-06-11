026aaa13b86396f2f9ab3b78fda9387edb537b65
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-11 12:45:01 +0330

    Grasp the functionality of all Lights | Ambient, Directional, Hemisphere, Point, RectArea, Spot Lights

--- src/script.js
+++ src/script.js
@@ -1,20 +1,139 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as THREE from 'three';
 import GUI from 'lil-gui';
+import { distance } from 'three/examples/jsm/nodes/Nodes.js';
 
 const canvas = document.querySelector('canvas.webgl'); // Canvas
 const scene = new THREE.Scene(); // Scene
 const gui = new GUI(); // Debug
 
 //======================= Lights ========================
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 50);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+directionalLight.position.set(1, 0.25, 0);
+scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+pointLight.position.set(1, -0.5, 1);
 scene.add(pointLight);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3()); // look at the "Center"
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(
+  0x78ff00,
+  0.5,
+  10,
+  Math.PI * 0.1,
+  0.25,
+  1
+);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
+
+const ambientLights = gui.addFolder('AmbientLight').close();
+const directionalLights = gui.addFolder('DirectionalLight').close();
+const hemisphereLights = gui.addFolder('HemisphereLight').close();
+const pointLights = gui.addFolder('PointLight').close();
+const rectAreaLights = gui.addFolder('RectAreaLight').close();
+const spotLights = gui.addFolder('SpotLight').close();
+
+ambientLights
+  .add(ambientLight, 'intensity')
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name('intensity');
+  
+directionalLights
+  .add(directionalLight, 'intensity')
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .name('intensity');
+
+hemisphereLights
+  .add(hemisphereLight, 'intensity')
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .name('intensity');
+
+pointLights
+  .add(pointLight, 'intensity')
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .name('intensity');
+pointLights
+  .add(pointLight, 'distance')
+  .min(0)
+  .max(20)
+  .step(0.01)
+  .name('distance');
+pointLights
+  .add(pointLight, 'decay')
+  .min(0)
+  .max(5)
+  .step(0.01)
+  .name('decay');
+
+rectAreaLights
+  .add(rectAreaLight, 'intensity')
+  .min(0)
+  .max(10)
+  .step(0.01)
+  .name('intensity');
+rectAreaLights
+  .add(rectAreaLight, 'width')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('width');
+rectAreaLights
+  .add(rectAreaLight, 'height')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('height');
+
+spotLights
+  .add(spotLight, 'intensity')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('intensity');
+spotLights
+  .add(spotLight, 'distance')
+  .min(0)
+  .max(15)
+  .step(0.001)
+  .name('distance');
+spotLights
+  .add(spotLight, 'angle')
+  .min(0)
+  .max(1)
+  .step(0.0001)
+  .name('angle');
+spotLights
+  .add(spotLight, 'penumbra')
+  .min(0)
+  .max(2)
+  .step(0.0001)
+  .name('penumbra');
+spotLights
+  .add(spotLight, 'decay')
+  .min(0)
+  .max(2)
+  .step(0.0001)
+  .name('decay');
 
 //======================= Objects ========================
 const material = new THREE.MeshStandardMaterial();
